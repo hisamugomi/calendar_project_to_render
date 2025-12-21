e60292ce149b144ea33f4691bbeb60f529c4a9f0
Hisamu Gomi <hisamug@gmail.com>
2025-12-21 22:20:49 +0900

    newinit branch

---
+++ cal.py
@@ -0,0 +1,45 @@
+import requests
+from icalendar import Calendar
+from datetime import datetime, date, time, timezone, timedelta
+import pandas as pd
+
+
+response = requests.get("https://calendar.google.com/calendar/ical/hisamug%40gmail.com/public/basic.ics")
+
+summaryone = []
+
+
+# change the date to datetime
+def to_datetime(dt_or_d):
+    if type(dt_or_d) is date:
+        print('date changed to datetime ', dt_or_d)
+        return datetime.combine(dt_or_d, time.min).astimezone(tz=timezone(timedelta(hours=9)))
+    else: return dt_or_d
+
+index = 0
+# 2. Parse it
+cal = Calendar.from_ical(response.content)
+
+print(cal)
+
+# 3. Loop through events
+for event in cal.walk('VEVENT'):
+    # 4. Get event data
+    summary = event.get('summary')
+    start = event.get('dtstart').dt
+    end = event.get('dtend').dt
+    summaryone.append({ 'starttime' : to_datetime(start), 'endtime': end,"eventname": summary})
+
+filtered = sorted(summaryone, key = lambda x: x['starttime'])
+df = pd.DataFrame(filtered)
+
+print(df)
+
+# def freetime(date):
+#     if x == date:
+#         listoftimes.append(x)
+
+# def figureoutfree(listoftimes):
+    
+# def rangeofdates(df):
+
---
+++ hello_app/views.py
@@ -0,0 +1,49 @@
+from django.shortcuts import render
+from datetime import datetime, timedelta, timezone, date
+
+busy_events =  [
+    {"start": datetime(2025, 10, 22, 10, 0, tzinfo=timezone.utc), "end": datetime(2025, 10, 22, 16, 30, tzinfo=timezone.utc), "label": "Busy"},
+    {"start": datetime(2025, 10, 22, 16, 0, tzinfo=timezone.utc), "end": datetime(2025, 10, 22, 17, 0, tzinfo=timezone.utc), "label": "Meeting"},
+    {"start": datetime(2025, 10, 22, 21, 0, tzinfo=timezone.utc), "end": datetime(2025, 10, 22, 13, 30, tzinfo=timezone.utc), "label": "✅ Coding"},
+]
+
+# Create your views here.
+def get_full_schedule(busy_events):
+    full_schedule = []
+    
+    for i in range(len(busy_events)):
+        # Add the busy event itself
+        full_schedule.append(busy_events[i])
+        
+        # 2. Check for a gap between this event and the next one
+        if i < len(busy_events) - 1:
+            current_end = busy_events[i]['end']
+            next_start = busy_events[i+1]['start']
+            
+            if next_start > current_end:
+                # We found a gap! Create a "Free" event
+                full_schedule.append({
+                    "start": current_end,
+                    "end": next_start,
+                    "label": "free",
+                    "type": "free"  # We'll use this for the CSS class
+                })
+        else : full_schedule.append({
+            "start": busy_events[i]['end'],
+            "end": datetime(2025, 10, 23, 20, 0, tzinfo=timezone.utc),
+            "label": "free",
+            "type": "free"  # We'll use this for the CSS class
+        })
+    return full_schedule
+
+def welcome_message():
+    message = f"Welcome Hisamu, \n {datetime.today().date()}"
+    return message
+
+def home(request):
+    context = {
+        'message': welcome_message(),
+        'full_schedule' : get_full_schedule(busy_events),
+        'status': 'Success'
+    }
+    return render(request, 'hello.html', context)
