538327eec1af51356ba68f674d82c89d172a4296
Hisamu Gomi <hisamug@gmail.com>
2025-12-26 11:42:44 +0900

    new req and setting

--- hello_app/views.py
+++ hello_app/views.py
@@ -9,12 +9,12 @@ busy_events =  [
 ]
 
 # Create your views here.
-def get_full_schedule(busy_events):
-    full_schedule = []
+def fill_schedule_free(busy_events):
+    day_schedule = []
     
     for i in range(len(busy_events)):
         # Add the busy event itself
-        full_schedule.append(busy_events[i])
+        day_schedule.append(busy_events[i])
         
         # 2. Check for a gap between this event and the next one
         if i < len(busy_events) - 1:
@@ -23,19 +23,19 @@ def get_full_schedule(busy_events):
             
             if next_start > current_end:
                 # We found a gap! Create a "Free" event
-                full_schedule.append({
+                day_schedule.append({
                     "start": current_end,
                     "end": next_start,
                     "label": "free",
                     "type": "free"  # We'll use this for the CSS class
                 })
-        elif busy_events[i]['end']: full_schedule.append({
+        elif busy_events[i]['end']: day_schedule.append({
             "start": busy_events[i]['end'],
             "end": datetime(2025, 10, 23, 20, 0, tzinfo=timezone.utc),
             "label": "free",
             "type": "free"  # We'll use this for the CSS class
         })
-    return full_schedule
+    return day_schedule
 
 def welcome_message():
     message = f"Welcome Hisamu, \n {datetime.today().date()}"
@@ -47,7 +47,7 @@ def home(request):
     context = {
         'message': welcome_message(),
         'free_times' : free_times,
-        'full_schedule' : get_full_schedule(busy_events),
+        'day_schedule' : get_full_schedule(busy_events),
         'status': 'Success'
     }
     return render(request, 'hello.html', context)
--- myprojectcalendar/settings.py
+++ myprojectcalendar/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,3 +119,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",  # Or wherever your CSS file is
+]
+
+# For production (you might not need this in development)
+STATIC_ROOT = BASE_DIR / "staticfiles"
